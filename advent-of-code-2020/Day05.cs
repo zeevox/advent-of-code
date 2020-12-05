d2d7dbdde5be465e0df3e4dd5c5ec240118aa046
Timothy Langer <zeevox.dev@gmail.com>
2020-12-05 05:25:33 +0000

    Add Day 5 Solution
    * Today's seemed rather straightforward, it was simply a matter of spotting
    * that the boarding seat is basically a binary string. In comparison to yesterday,
    * part two was a piece of cake! My time definitely could've been better.
    *
    * Day       Time   Rank  Score       Time   Rank  Score
    *   5   00:18:01   3163      0   00:24:15   2925      0
    
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

---
+++ advent-of-code-2020/Day05.cs
@@ -0,0 +1,61 @@
+using System;
+using System.IO;
+using AoCHelper;
+
+namespace advent_of_code_2020
+{
+    public sealed class Day05 : BaseDay
+    {
+        private readonly string[] _input;
+        
+        public Day05()
+        {
+            _input = File.ReadAllLines(InputFilePath);
+        }
+
+        public int GetSeatId(bool partTwo = false)
+        {
+            var max = 0;
+            
+            bool[] array = null;
+            if (partTwo)
+            {
+                array = new bool[128 * 8];
+            }
+
+            foreach (var line in _input)
+            {
+                var row = Convert.ToInt32(line.Substring(0, 7).Replace("B", "1").Replace("F", "0"), 2);
+                var col = Convert.ToInt32(line.Substring(7).Replace("R", "1").Replace("L", "0"), 2);
+               
+                var id = row * 8 + col;
+
+                if (partTwo) array[id] = true;
+                
+                Console.WriteLine($"{line} -> {col}:{row} -> {id}");
+
+                if (id <= max) continue;
+                max = id;
+            }
+
+            if (!partTwo) return max;
+            
+            for (var i = 1; i < array.Length - 1; i++)
+            {
+                if (array[i - 1] && array[i + 1] && !array[i]) return i;
+            }
+
+            return 0;
+        }
+
+        public override string Solve_1()
+        {
+            return GetSeatId().ToString();
+        }
+
+        public override string Solve_2()
+        {
+            return GetSeatId(true).ToString();
+        }
+    }
+}
