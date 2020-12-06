66d08f6d13f9bc05b40c66b23aba79c13b90f36f
Timothy Langer <zeevox.dev@gmail.com>
2020-12-06 06:07:17 +0000

    Clean up Day 6 Solution
    * Remove all the junk commented code that my brain produced earlier today
    
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

--- advent-of-code-2020/Day06.cs
+++ advent-of-code-2020/Day06.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using AoCHelper;
@@ -9,7 +8,7 @@ namespace advent_of_code_2020
     public sealed class Day06 : BaseDay
     {
         private readonly string[] _input;
-        
+
         public Day06()
         {
             _input = File.ReadAllText(InputFilePath).Split(new[] {Environment.NewLine + Environment.NewLine},
@@ -20,42 +19,16 @@ namespace advent_of_code_2020
         {
             var count = 0;
             foreach (var group in _input)
-            {
                 if (partTwo)
                 {
                     var splitGroup = group.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
-                    Console.WriteLine($"{String.Join(", ", splitGroup)}");
-                    var uniqueQs = splitGroup[0].ToCharArray().AsEnumerable();
-                    foreach (var person in splitGroup)
-                    {
-                        uniqueQs = uniqueQs.Intersect(person.ToCharArray());
-                    }
-
-                    Console.WriteLine($"{String.Join(", ", uniqueQs)} -> {uniqueQs.Count()}");
-
-                    count += uniqueQs.Count();
-
-                    // var splitGroup = group.Split(Environment.NewLine);
-                    // splitGroup.Select(s => s.ToCharArray()).Aggregate(splitGroup[0].ToCharArray().AsEnumerable(),
-                    //     (a, chars) => a.Intersect(chars), a => a);
-                    // Console.WriteLine(String.Join(", ", splitGroup.ToArray()));
-                    // splitGroup.Select(s => s.Split()).Aggregate(splitGroup[0].Split().AsEnumerable(),
-                    //     (a, words) => a.Intersect(words), a => a);
-                    // var questions = new List<char>();
-                    // foreach (var repeatedString in splitGroup)
-                    // {
-                    //     questions.AddRange(repeatedString.ToCharArray());
-                    // }
-                    //
-                    // Console.WriteLine(String.Join(", ", questions.ToArray()));
-                    //count += questions.Distinct().Count();
+                    count += splitGroup.Aggregate(splitGroup[0].ToCharArray().AsEnumerable(),
+                        (current, person) => current.Intersect(person.ToCharArray())).Count();
                 }
                 else
                 {
                     count += group.Replace(Environment.NewLine, "").Distinct().Count();
-
                 }
-            }
 
             return count;
         }
