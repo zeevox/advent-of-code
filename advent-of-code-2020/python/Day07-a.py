4c48324b54f3e7419c6c1018ef37458705b1cac4
Timothy Langer <zeevox.dev@gmail.com>
2020-12-07 06:43:29 +0000

    Light cleanup of Day 7 [Python]
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

--- advent-of-code-2020/python/Day07-a.py
+++ advent-of-code-2020/python/Day07-a.py
@@ -12,22 +12,22 @@ def main():
     inp = [re.split(" bags contain |, ", line) for line in aoc_utils.input_string_list()]
     di = {line[0]: line[1:] for line in inp}
     count = 0
-    for bagcolour in (i[0] for i in inp):
-        if cangettoshinygold(bagcolour, di):
+    for start_colour in (i[0] for i in inp):
+        if route_possible(start_colour, di):
             count += 1
     return count
 
-def cangettoshinygold(bagcolour, di):
+def route_possible(start_colour, end_colour, di):
     q = queue.Queue()
-    q.put([bagcolour] + di[bagcolour])
+    q.put([start_colour] + di[start_colour])
     while not q.empty():
         bag = q.get()
-        if "shiny gold" in ", ".join(bag[1:]):
-            print(bag[0])
+        if end_colour in ", ".join(bag[1:]):
             return True
         else:
             if "no other bags" not in ", ".join(bag[1:]):
                 for containable in bag[1:]:
+                    # this regex gets the bag colour, which is always two words long.
                     if match := re.search(r"(?i)\d* (\w+ \w+).*", containable):
                         q.put([match.group(1)] + di[match.group(1)])
     return False
--- advent-of-code-2020/python/Day07-b.py
+++ advent-of-code-2020/python/Day07-b.py
@@ -14,29 +14,14 @@ def main():
 
 def bagscontain(bagcolour, inp):
     di = {line[0]: line[1:] for line in inp}
-    #print(*inp, sep="\n")
     count = 0
     bag = [bagcolour] + di[bagcolour]
     subbags = []
     if "no other bags" not in ", ".join(bag[1:]):
         for containable in bag[1:]:
             if match := re.search(r"(?i)\d* (\w+ \w+).*", containable):
-                #print(match.group(1))
                 count += int(containable.split(" ")[0]) * (1 + bagscontain(match.group(1), inp))
                 subbags.append([match.group(1)] + di[match.group(1)])
     return count
-    #count += sum([bagscontain(])
-    #q = queue.Queue()
-    #q.put([bagcolour] + di[bagcolour])
-    #while not q.empty():
-        #print(list(q.queue))
-        #bag = q.get()
-        #print(bag)
-        #print(bag, ", ".join(bag[1:]))
-        #if "shiny gold" in ", ".join(bag[1:]):
-        #    print(bag[0])
-        #    return True
-        #else:
-    #return False
 
 print(main())
