06c78b2a8e5104b70f0470b357b4bc188a217f5e
Timothy Langer <zeevox.dev@gmail.com>
2020-12-09 11:19:10 +0000

    Stop bash interpreting numbers
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

--- python/setup.sh
+++ python/setup.sh
@@ -2,7 +2,7 @@
 if [ $1 -eq 0 ]; then
     day=$(date +%d)
 else
-    day=$(printf %02d $1)
+    day=$(printf %02d "$1")
 fi
 cp --no-clobber base.py "Day${day}.py"
 ln=$(( $(grep --fixed-strings --line-number 'def main(' "Day${day}.py" | cut --delimiter=: --fields=1)+1 ))
