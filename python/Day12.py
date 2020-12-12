7094961e39c31de1363c8af9f3f748589c935124
Timothy Langer <zeevox.dev@gmail.com>
2020-12-12 05:21:21 +0000

    Add Day 12 Solution
    * This was a very quick question to solve, albeit a bunch of 'if statements'
    * Pretty pleased with my global performance too:
    *
    * Day       Time   Rank  Score       Time   Rank  Score
    *  12   00:08:56    598      0   00:20:08    685      0
    
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

---
+++ python/Day12.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+from collections import *
+import itertools
+import random
+import re
+import sys
+import aoc_utils
+
+ins = aoc_utils.input_string_list()
+sx, sy, d = 0, 0, 90
+wx, wy = 10, 1
+for i in ins:
+    c = i[0]
+    n = int(i[1:])
+    if c == "N":
+        wy += n
+    elif c == "S":
+        wy -= n
+    elif c =="E":
+        wx += n
+    elif c == "W":
+        wx -= n
+    elif (c == "L" and n == 90) or (c == "R" and n == 270):
+        wx, wy = -wy, wx
+    elif (c == "L" or c == "R") and n == 180:
+        wx, wy = -wx, -wy
+    elif (c == "L" and n == 270) or (c == "R" and n == 90):
+        wx, wy = wy, -wx
+    elif c == "F":
+        sx += n * wx
+        sy += n * wy
+    print(sx, sy, d)
+print(abs(sx)+abs(sy))
---
+++ python/Day12a.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+from collections import *
+import itertools
+import random
+import re
+import sys
+import aoc_utils
+
+ins = aoc_utils.input_string_list()
+x, y, d = 0, 0, 90
+for i in ins:
+    c = i[0]
+    n = int(i[1:])
+    if c == "N":
+        y += n
+    elif c == "S":
+        y -= n
+    elif c =="E":
+        x += n
+    elif c == "W":
+        x -= n
+    elif c == "L":
+        d -= n
+        if d < 0:
+            d += 360
+    elif c == "R":
+        d += n
+        if d >= 360:
+            d -= 360
+    elif c == "F":
+        if d == 0:
+            y += n
+        elif d == 90:
+            x += n
+        elif d == 180:
+            y -= n
+        elif d == 270:
+            x -= n
+    print(x, y, d)
+print(abs(x)+abs(y))
