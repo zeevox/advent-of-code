04d71304ea2b52fc4974fad9ac2f5bdedabd44ed
Timothy Langer <zeevox.dev@gmail.com>
2020-12-14 05:43:59 +0000

    Add Day 14 Solution
    * Working with bitstrings; pretty straightforward stuff
    *
    * Day       Time   Rank  Score       Time   Rank  Score
    *  14   00:18:53   1347      0   00:39:34   1149      0
    
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

---
+++ python/Day14.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+from collections import *
+import itertools
+import random
+import re
+import sys
+import aoc_utils
+
+mask = ""
+mem = {}
+for line in aoc_utils.input_string_list():
+    i, v = line.split(" = ")
+    if i == "mask":
+        mask = v
+    elif i[:3] == "mem":
+        v = int(v)
+        adr = int(i[4:-1])
+        out = ""
+        xs = []
+        # 36 bits - hardcoded value, iterate over mask and bits simultaneously
+        for i, (bv, bm) in enumerate(zip(list(f'{adr:b}'.zfill(36)), list(mask))):
+            if bm == "X":
+                out += "{}"
+                xs.append(i)
+            elif bm == "0":
+                out += bv
+            elif bm == "1":
+                out += "1"
+        # e.g. if there are 3 floating bits there are 2^3=8 combinations
+        for i in range(2 ** len(xs)):
+            # replace each {} with one of the bits from the binary number
+            mem[int(out.format(*list(f"{i:b}".zfill(len(xs)))), 2)] = v
+
+print(sum(mem.values()))
+
---
+++ python/Day14a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+from collections import *
+import itertools
+import random
+import re
+import sys
+import aoc_utils
+
+mask = ""
+mem = {}
+for line in aoc_utils.input_string_list():
+    i, v = line.split(" = ")
+    #print(i,v, mem)
+    if i == "mask":
+        mask = v
+    elif i[:3] == "mem":
+        v = int(v)
+        adr = int(i[4:-1])
+        out = ""
+        for bv, bm in zip(list(f'{v:b}'.zfill(36)), list(mask)):
+            out += bv if bm == "X" else bm
+        #print(f"{mask}\n{v:36b}\t{v}\n{out}")
+        mem[adr] = int(out, 2)
+
+
+print(sum(mem.values()))
+
