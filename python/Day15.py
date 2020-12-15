ccec61f5da729b2a1ef9a4523cbe1af6af878d4c
Timothy Langer <zeevox.dev@gmail.com>
2020-12-15 05:59:42 +0000

    Add Day 15 Solution
    * More efficient than using a list of 30 million values
    * but still took 47 seconds to compute part 2
    *
    * Day       Time   Rank  Score       Time   Rank  Score
    *  15   00:16:11   1428      0   00:27:13   1597      0
    
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

---
+++ python/Day15.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from collections import *
+import itertools
+import random
+import re
+import sys
+import aoc_utils
+
+
+li = [1,0,15,2,10,13]
+freq = {x: [i+1, i+1] for i, x in enumerate(li)}
+
+counter = len(li)+1
+last = li[counter-2]
+while counter <= 30000000:
+    p = freq[last]
+    if p[0] == p[1]:
+        v = 0
+    else:
+        v = p[1] - p[0]
+    last = v
+    freq[v] = [freq.get(v, [counter])[-1], counter]
+    #just to get an idea of how much progress we were making
+    #print(counter/30000000*100)
+    counter += 1
+
+
+print(last)
---
+++ python/Day15a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+from collections import *
+import itertools
+import random
+import re
+import sys
+import aoc_utils
+
+
+li = [1,0,15,2,10,13]
+freq = {x: [i+1, i+1] for i, x in enumerate(li)}
+
+counter = len(li)+1
+while counter <= 2020:
+    p = freq[li[counter-2]]
+    if p[0] == p[1]:
+        v = 0
+    else:
+        v = p[1] - p[0]
+    li.append(v)
+    freq[v] = [freq.get(v, [counter])[-1], counter]
+    counter += 1
+
+
+print(li[-1])
