6afbdf5f5483ea8952597d9dcd30352a50312dd7
Timothy Langer <zeevox.dev@gmail.com>
2020-12-16 07:28:35 +0000

    Add Day 16 Solution
    * Had a lie in for the first day of the holidays, got up at 06:20!
    * But a pretty decent delta time of 00:34:31 nevertheless:
    *
    * Day       Time   Rank  Score       Time   Rank  Score
    *  16   01:43:47   6951      0   02:18:18   4671      0
    
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

---
+++ python/Day16.py
@@ -0,0 +1,67 @@
+##!/usr/bin/python3
+
+#from collections import *
+#import itertools
+#import random
+import re
+#import sys
+import aoc_utils
+
+inp = aoc_utils.input_string_list()
+rules = {(x := re.split(": |-| or ", s))[0]: list(map(int,x[1:])) for s in inp[:20]}
+#print(rules)
+
+def valid_any(n):
+    for rule in rules.values():
+#        print(n, rule)
+        if valid(n, rule):
+            return True
+    return False
+
+def valid(n, rule):
+    return ((n >= rule[0] and n <= rule[1]) or (n >= rule[2] and n <= rule[3]))
+
+sum = 0
+ts = inp[inp.index("nearby tickets:")+1:]
+for ticket in inp[inp.index("nearby tickets:")+1:]:
+    for value in map(int,ticket.split(",")):
+        if not valid_any(value):
+#            print(value)
+            sum += value
+            ts.remove(ticket)
+
+print(sum)
+
+ts = [list(map(int,ticket.split(","))) for ticket in ts]
+
+cols = {}
+myticket = list(map(int,inp[inp.index("your ticket:")+1].split(",")))
+for n, rule in rules.items():
+    #if n[:9] != "departure":
+    #    continue
+    for col in range(len(ts[0])):
+        if all(valid(int(t[col]),rule) for t in ts):
+            cols[n] = cols.get(n, []) + [col]
+
+#print(cols)
+
+fixed =[]
+while not len(fixed) == len(cols):
+    for n, col in cols.items():
+        if len(col) == 1:
+            if col[0] not in fixed:
+                fixed.append(col[0])
+        else:
+            for f in fixed:
+                if f in col:
+                    col.remove(f)
+    #print(cols, fixed)
+#print(cols)
+
+
+p = 1
+for k, v in cols.items():
+    if k[:9] == "departure":
+        p *= myticket[v[0]]
+
+print(p)
