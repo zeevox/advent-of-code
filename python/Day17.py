46cfe6f370f550887a038daa73442a0b5cac68a8
Timothy Langer <zeevox.dev@gmail.com>
2020-12-18 08:00:55 +0000

    Clean up Day 17 Solution
    * More efficient solution than waiting 15 minutes
    * Analyse only active (hyper)cubes and ones adjacent to them
    
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

--- python/Day17.py
+++ python/Day17.py
@@ -1,59 +1,43 @@
-#!/usr/bin/python3
-
-from collections import *
+from operator import add
+from copy import deepcopy
 import itertools
-import random
-import re
-import sys
 import aoc_utils
 
-data = []
+dimensions = 4
 
-class Point():
-    def __init__(self, x, y, z, w, state):
-        self.x, self.y, self.z, self.w, self.state = x, y, z, w, state
+# use a set instead of a list for O(1) `x in s` lookups
+# https://wiki.python.org/moin/TimeComplexity
+d = set()
 
-def get_neighbours(x, y, z, w):
-    # optimised neighbour-finding process
-    neighbours = []
-    for p in data:
-        if abs(p.x-x)<= 1 and abs(p.y-y)<=1 and abs(p.z-z)<= 1 and abs(p.w-w)<= 1 and not (p.x, p.y, p.z, p.w) == (x, y, z, w):
-            neighbours.append(p)
-    return neighbours
-    # this works too, took 15 minutes though
-    #coords = [(x + i, y + j, z + k, w + l) for i in [-1, 0, 1] for j in [-1, 0, 1] for k in [-1, 0, 1] for l in [-1, 0, 1] if not (i, j, k, l) == (0, 0, 0, 0)]
-    #return [p for p in data if (p.x, p.y, p.z, p.w) in coords]
-
-def value_at(x, y, z, w):
-    point = [p for p in data if (p.x, p.y, p.z, p.w) == (x, y, z, w)]
-    return point[0].state if len(point) == 1 else False
-
-inp = aoc_utils.input_string_list()
-for j, line in enumerate(inp):
-    for i, col in enumerate(line):
+for y, line in enumerate(aoc_utils.input().readlines()):
+    for x, col in enumerate(line):
         if col == "#":
-            data.append(Point(i, j, 0, 0, True))
+            d.add(tuple([x, y] + [0] * (dimensions - 2)))
 
-mx, my, mz, mw = (0, len(inp[0])), (0, len(inp)), (0,1), (0,1)
+deltas = set(itertools.product([-1, 0, 1], repeat=dimensions))
+deltas.remove(tuple([0] * dimensions))
 
-def cycle(mx, my, mz, mw):
-    ndata = []
-    for x in range(*mx):
-        for y in range(*my):
-            for z in range(*mz):
-                for w in range(*mw):
-                    neighbours = get_neighbours(x, y, z, w)
-                    val = value_at(x, y, z, w)
-                    #print(x, y, z, val, neighbours)
-                    if (val == True and (len(neighbours) == 2 or len(neighbours) == 3)) or (val == False and len(neighbours) == 3):
-                        ndata.append(Point(x, y, z, w, True))
-    return ndata
+def cycle():
+    data = deepcopy(d)
+    changeable = set()
+    for point in data:
+        changeable.add(point)
+        for delta in deltas:
+            changeable.add(tuple(map(add, point, delta)))
 
-#print([(p.x, p.y, p.z) for p in data])
+    for point in changeable:
+        neighbours = 0
+        for delta in deltas:
+            if tuple(map(add, point, delta)) in d:
+                neighbours += 1
+        if not (neighbours == 2 or neighbours == 3):
+            data.discard(point)
+        else:
+            if neighbours == 3: # and point in d:
+                data.add(point)
+    return data
 
-for i in range(6):
-    mx, my, mz, mw = (mx[0]-1, mx[1]+1), (my[0]-1, my[1]+1), (mz[0]-1, mz[1]+3), (mw[0]-1, mw[1]+3)
-    data = cycle(mx, my, mz, mw)
-#    print([(p.x, p.y, p.z) for p in data])
+for _ in range(6):
+    d = cycle()
 
-print(len(data))
+print(len(d))
