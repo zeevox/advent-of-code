adfc71c587d7a83301fefe5b82791bdabbfe17bc
Timothy Langer <zeevox.dev@gmail.com>
2020-12-23 09:05:48 +0000

    Add Day 23 Solution
    * If I had reused my code from part 1, it would have taken a week to brute force
    * part 2. So I tried turning the list into a deque, thus optimising addition and
    * removal of elements from start/end, bringing it down to around two days.
    *
    * In other words, I had to completely rethink my approach for part 2. Ended up
    * using a linked list, noting that we could point to the previous (by value)
    * item since it would never change, thus saving mega time on searching for it.
    *
    * Day       Time   Rank  Score       Time   Rank  Score
    *  23   01:17:41   2980      0   03:53:14   2628      0
    
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

---
+++ python/Day23a.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+
+from collections import *
+import itertools
+import random
+import re
+import sys
+import aoc_utils
+
+
+def inp():
+    return int(aoc_utils.input().read())
+
+def destination(search, cups):
+    while 1:
+        if search in cups:
+            return cups.index(search)
+        search -= 1
+        if search < min(cups):
+            search = max(cups)
+
+def move(current, cups):
+    current_card = cups[current]
+
+    picked = []
+    inv = -1 * (len(cups) - current)
+    for i in range(1, 4):
+        if inv + i >= 0:
+            pick = cups.pop(0)
+        else:
+            pick = cups.pop(inv + i)
+        picked.append(pick)
+
+    print("pick up:", *picked)
+
+    dest = destination(current_card - 1, cups)
+    print("destination:", cups[dest])
+
+    new_cups = cups[:dest + 1] + picked + cups[dest + 1:]
+    new_current = (current + 1) % len(new_cups)
+
+    if dest < current:
+        for _ in range(min(3, len(new_cups) - (current + 1))):
+            new_cups.append(new_cups.pop(0))
+
+    return new_current, new_cups
+
+def print_cups(current, cups):
+    print("cups:", end=" ")
+    for i, c in enumerate(cups):
+        print(f"({c})" if i == current else c, end=" ")
+    print()
+
+def output(cups):
+    print(*cups[cups.index(1)+1:], *cups[:cups.index(1)], sep="")
+
+if __name__ == "__main__":
+    current = 0
+    #cups = list(map(int, list("389125467")))
+    cups = list(map(int, list("318946572")))
+    print(current, cups)
+
+    for i in range(100):
+        print(f"\n-- move {i+1} --")
+        print_cups(current, cups)
+        current, cups = move(current, cups)
+
+    print("\n-- final --")
+    print_cups(current, cups)
+    output(cups)
---
+++ python/Day23b.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+class cup:
+    def __init__(self, label):
+        self.label = int(label)
+
+    def link(self, next, previous_by_label):
+        self.next, self.previous_by_label = next, previous_by_label
+
+
+# get the cups, then pad with numbers
+def get_cups(s, maxlen):
+    yield from map(cup, list(s))
+    yield from map(cup, range(len(s)+1, maxlen+1))
+
+def process_cups(s, n_cups = 1000000):
+
+    # perhaps not the most efficient way of generating a linked list, but it works
+    cups = list(get_cups(s, n_cups))
+    cups_by_value = sorted(cups, key=lambda x: x.label)
+
+    for i in range(len(cups)):
+        this_cup = cups[i]
+        next_cup = cups[(i + 1) % len(cups)]
+        prev_cup = cups_by_value[this_cup.label - 2]
+
+        this_cup.link(next_cup, prev_cup)
+    return cups[0]
+
+def move(current_cup):
+    next_three = [current_cup.next, current_cup.next.next, current_cup.next.next.next]
+
+    destination_cup = current_cup.previous_by_label
+    while destination_cup in next_three or current_cup == destination_cup:
+        destination_cup = destination_cup.previous_by_label
+
+    current_cup.next = next_three[2].next
+    destination_cup.next, next_three[2].next = next_three[0], destination_cup.next
+
+    return current_cup.next
+
+current_cup = process_cups("318946572")
+for i in range(10000000):
+    current_cup = move(current_cup)
+
+while current_cup.label != 1:
+    current_cup = current_cup.next
+
+print(current_cup.next.label * current_cup.next.next.label)
