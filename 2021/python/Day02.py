45cc95716ed1bfde80fb6011942093420bcf10c6
Timothy Langer <zeevox.dev@gmail.com>
2021-12-02 05:05:36 +0000

    Solve 2021 Day 2 in Python
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

---
+++ 2021/python/Day02.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import itertools
+import random
+import re
+import sys
+from collections import *
+
+import aoc_utils
+
+
+def part1():
+    xs = aoc_utils.input_string_list()
+    x, z = 0, 0
+    for i in xs:
+        c, d = i.split()
+        if c == "forward":
+            x += int(d)
+        elif c == "down":
+            z -= int(d)
+        elif c == "up":
+            z += int(d)
+    print(abs(x) * abs(z))
+
+
+def part2():
+    xs = aoc_utils.input_string_list()
+    x, z, aim = 0, 0, 0
+    for i in xs:
+        c, d = i.split()
+        if c == "forward":
+            x += int(d)
+            z -= aim * int(d)
+        elif c == "down":
+            aim -= int(d)
+        elif c == "up":
+            aim += int(d)
+    print(abs(x) * abs(z))
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
