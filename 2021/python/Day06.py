c8326bd163dd61353397fede1d2847e83df00022
Timothy Langer <zeevox.dev@gmail.com>
2021-12-06 05:50:48 +0000

    Solve 2021 Day 6 in Python
    * Fat fingers are fat, typed 5 instead of 6 and spent wayyyy too long
      headbashing as to why it wasn't working
    * But I anticipated that brute force list was not the way to go because
      part 2 would ramp up requirements, so a very good delta obtained
    
          --------Part 1--------   -------Part 2--------
    Day       Time   Rank  Score       Time  Rank  Score
      6   00:46:05  10216      0   00:46:19  5552      0
    
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

---
+++ 2021/python/Day06.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from collections import *
+import itertools
+import random
+import re
+import sys
+import aoc_utils
+
+
+if __name__ == "__main__":
+    # nine slots for fish aged 0-8
+    fishes = deque([0] * 9)
+    # each slot is a counter of the number fish of that age
+    for fish in map(int, aoc_utils.input_string_list()[0].split(",")):
+        fishes[fish] += 1
+    # repeat the process lots of times (80 for first part, 256 for second)
+    for i in range(256):
+        # those fish that were aged zero were at the leftmost end of the list
+        fishes_to_reset = fishes.popleft()
+        # these fish are now aged six and their children aged eight (logik)
+        fishes[6] += fishes_to_reset
+        fishes.append(fishes_to_reset)
+    # all we need is the total number of such fish
+    print(sum(fishes))
