9b41b1a305d5210102a85acafeea34aaf53ffd5c
Timothy Langer <zeevox.dev@gmail.com>
2021-12-12 16:15:56 +0000

    Solve 2021 Day 12 in Python
    * Misreading the question cost me significant time debugging
    * Once I understood what was required of me it worked very quickly
    
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

---
+++ 2021/python/Day12.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+import queue
+import aoc_utils
+import networkx as nx
+
+
+def count_double_lowers(path: list[str]):
+    count = 0
+    for n in path:
+        if not n.islower():
+            continue
+        if path.count(n) >= 2:
+            count += 1
+    return count
+
+def main(lines: list[str]):
+    g = nx.Graph()
+    for line in lines:
+        start_node, end_node = line.strip().split("-")
+        g.add_edge(start_node, end_node)
+    q = queue.Queue()
+    q.put(["start"])
+    paths = []
+    while not q.empty():
+        node = q.get()
+        if node[-1] == "end":
+            paths.append(node)
+            continue
+        for n in g.neighbors(node[-1]):
+            if n == "start" or (n.islower() and n in node):
+                continue
+            q.put(node + [n])
+    print(len(paths))
+
+
+    q = queue.Queue()
+    q.put(["start"])
+    paths = []
+    while not q.empty():
+        node = q.get()
+        if node[-1] == "end":
+            paths.append(node)
+            continue
+        for n in g.neighbors(node[-1]):
+            if n == "start" or (n.islower() and count_double_lowers(node) > 0 and n in node):
+                continue
+            q.put(node + [n])
+    print(len(paths))
+
+
+if __name__ == "__main__":
+    main(aoc_utils.input_string_list())
