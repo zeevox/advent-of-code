8167b3eb1463f55462623a102fdd68877a37ab8f
Timothy Langer <zeevox.dev@gmail.com>
2021-12-16 17:25:52 +0000

    Clean up 2021 Day 16
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

--- 2021/python/Day16v3.py
+++ 2021/python/Day16v3.py
@@ -2,12 +2,9 @@
 
 import dataclasses
 import math
-import operator
-import queue
 from typing import Optional
 
 import aoc_utils
-
 
 hex_lookup = {
     "0": "0000",
@@ -37,49 +34,56 @@ def hex2bin(s: str):
 class Packet:
     version: int
     type: int
-    data: str
     value: Optional[int] = None
     extra: Optional[str] = None
-    subpackets: Optional[list] = None
+    subpackets: Optional[list["Packet"]] = None
 
     def __init__(self, bin_str: str) -> None:
         self.version = int(bin_str[:3], 2)
         self.type = int(bin_str[3:6], 2)
-        self.data = bin_str[6:]
+        data = bin_str[6:]
 
         if self.type == 4:
             value = ""
-            for i in range(0, len(self.data), 5):
-                value += self.data[i + 1 : i + 5]
-                if self.data[i] == "0":
+            for i in range(0, len(data), 5):
+                value += data[i + 1 : i + 5]
+                if data[i] == "0":
                     break
             self.value = int(value, 2)
-            self.extra = self.data[i + 5 :]
-            self.data = self.data[: i + 5]
-        else:
-            self.subpackets = []
-            self.length_type = self.data[0]
-            self.data = self.data[1:]
-            unparsed_data = ""
-            if self.length_type == "0":
-                bit_count = int(self.data[:15], 2)
-                unparsed_data = self.data[15 : 15 + bit_count]
-                self.extra = self.data[15 + bit_count :]
-                while len(unparsed_data) > 0:
-                    subpacket = Packet(unparsed_data)
-                    unparsed_data = subpacket.extra
-                    self.subpackets.append(subpacket)
-            elif self.length_type == "1":
-                number_of_subpackets = int(self.data[:11], 2)
-                unparsed_data = self.data[11:]
-                while len(self.subpackets) < number_of_subpackets:
-                    subpacket = Packet(unparsed_data)
-                    unparsed_data = subpacket.extra
-                    self.subpackets.append(subpacket)
-                self.extra = unparsed_data
-            self.data = self.data[: ~len(self.extra)]
+            self.extra = data[i + 5 :]
+            data = data[: i + 5]
+            return
+
+        # all other packets are operator packets and
+        # thus have subpackets contained within them
+        self.subpackets = []
+        self.length_type = data[0]
+
+        # for easier indexing later on remove 1st char
+        data = data[1:]
+
+        # length type 0 provides the number of bits to read
+        if self.length_type == "0":
+            bit_count = int(data[:15], 2)
+            unparsed_data = data[15 : 15 + bit_count]
+            self.extra = data[15 + bit_count :]
+            while len(unparsed_data) > 0:
+                subpacket = Packet(unparsed_data)
+                unparsed_data = subpacket.extra
+                self.subpackets.append(subpacket)
+
+        # length type 1 provides the number of subpackets to read
+        elif self.length_type == "1":
+            number_of_subpackets = int(data[:11], 2)
+            unparsed_data = data[11:]
+            while len(self.subpackets) < number_of_subpackets:
+                subpacket = Packet(unparsed_data)
+                unparsed_data = subpacket.extra
+                self.subpackets.append(subpacket)
+            self.extra = unparsed_data
 
     def version_sum(self) -> int:
+        """add up the version numbers in all packets"""
         return self.version + (
             sum(p.version_sum() for p in self.subpackets)
             if self.subpackets is not None
@@ -87,6 +91,7 @@ class Packet:
         )
 
     def evaluate(self) -> int:
+        """evaluate the expression represented by the BITS transmission"""
         if self.type == 0:
             return sum(p.evaluate() for p in self.subpackets)
         elif self.type == 1:
