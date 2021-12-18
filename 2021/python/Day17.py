80192178055e961a886b5c7bb10b70b62e03c24a
Timothy Langer <zeevox.dev@gmail.com>
2021-12-18 04:55:45 +0000

    Solve 2021 Day 17 in Python
    * Just in time for day 18. No prizes for day 17 for me seeing as I slept
      through my alarm and didn't complete till much later.
    
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

---
+++ 2021/python/Day17.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+import parse
+import itertools
+
+import aoc_utils
+
+
+def triangle(n: int):
+    return n * (n + 1) // 2
+
+
+def gen_x_coords(v_x: int):
+    max_x = triangle(v_x)
+    yield from (max_x - triangle(n) for n in range(v_x, -1, -1))
+
+
+def gen_initial_vx(x_max: int):
+    for v_x in range(x_max + 1):
+        for x in gen_x_coords(v_x):
+            if x_min <= x <= x_max:
+                yield v_x
+            if x >= x_min:
+                break
+
+
+if __name__ == "__main__":
+    data = aoc_utils.input_string()
+    x_min, x_max, y_min, y_max = parse.parse(
+        "target area: x={:d}..{:d}, y={:d}..{:d}", data
+    )
+
+    print("Part 1:", triangle(y_min))
+
+    total = 0
+    for v_iy in range(y_min, -y_min):
+        for v_ix in gen_initial_vx(x_max):
+            x, y = 0, 0
+            v_x, v_y = v_ix, v_iy
+            while x <= x_max and y >= y_min:
+                x, y = x + v_x, y + v_y
+                if y_min <= y <= y_max and x_min <= x <= x_max:
+                    total += 1
+                    break
+                v_y -= 1
+                if v_x > 0:
+                    v_x -= 1
+    print("Part 2:", total)
