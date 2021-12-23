35216a36262f9a7d78b2e80730750956859f1cd7
Timothy Langer <zeevox.dev@gmail.com>
2021-12-23 06:43:23 +0200

    Solve 2021 Day 21 in Python
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

---
+++ 2021/python/Day21.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import aoc_utils
+
+from parse import parse
+
+def main(a, b):
+    rolls = 0
+    players = [0, 0]
+    players_pos = [a - 1, b - 1]
+    won = False
+    while not won:
+        for i in range(len(players)):
+            for _ in range(3):
+                rolled = rolls % 100 + 1
+                players_pos[i] = (players_pos[i] + rolled) % 10
+                rolls += 1
+            players[i] += players_pos[i] + 1
+            print(
+                f"Player {i+1} moves to space {players_pos[i] + 1} for a total score of {players[i]}"
+            )
+            if players[i] >= 1000:
+                won = True
+                break
+    print(f"{rolls} * {min(players)} = {rolls * min(players)}")
+
+
+if __name__ == "__main__":
+    string = aoc_utils.input_string()
+    # string = "Player 1 starting position: 4\nPlayer 2 starting position: 8"
+    a, b = parse(
+        "Player 1 starting position: {:d}\nPlayer 2 starting position: {:d}", string
+    )
+    main(a, b)
---
+++ 2021/python/Day21b.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import functools
+
+import parse
+import aoc_utils
+
+outcomes = {3: 1, 4: 3, 5: 6, 6: 7, 7: 6, 8: 3, 9: 1}
+
+
+@functools.cache
+def main(a_pos, b_pos, a_score=0, b_score=0):
+    if a_score >= 21:
+        return 1, 0
+    elif b_score >= 21:
+        return 0, 1
+
+    a_wins = 0
+    b_wins = 0
+
+    for rolled, count in outcomes.items():
+        new_a_pos = (a_pos + rolled) % 10
+        new_a_score = a_score + new_a_pos + 1
+
+        b_subwins, a_subwins = main(b_pos, new_a_pos, b_score, new_a_score)
+
+        a_wins += a_subwins * count
+        b_wins += b_subwins * count
+
+    return a_wins, b_wins
+
+
+if __name__ == "__main__":
+    string = aoc_utils.input_string()
+    a, b = parse.parse(
+        "Player 1 starting position: {:d}\nPlayer 2 starting position: {:d}", string
+    )
+    print(max(main(a - 1, b - 1)))
