f2870c3b9d455f1068956c911c9af13d2fa65196
Timothy Langer <zeevox.dev@gmail.com>
2021-12-29 00:34:46 +0000

    Solve 2021 Day 18 in Python
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

---
+++ 2021/python/Day18v2.py
@@ -0,0 +1,183 @@
+#!/usr/bin/python3
+
+import itertools
+from pprint import pprint
+import re
+
+import aoc_utils
+
+
+class Node:
+    left = None
+    right = None
+    parent = None
+
+    def __init__(self, parent, data) -> None:
+        self.parent = parent
+
+        if isinstance(data, int):  # leaf node
+            self.left = data
+            return
+
+        # otherwise data must be two-element tuple
+        left, right = data
+        self.left, self.right = Node(self, left), Node(self, right)
+
+    def from_string(string: str) -> "Node":
+        return Node(None, eval(string))
+
+    def depth(self):
+        return 1 if self.parent is None else self.parent.depth() + 1
+
+    def is_leaf(self):
+        return self.right is None
+
+    def is_pair(self):
+        return (not self.is_leaf()) and self.left.is_leaf() and self.right.is_leaf()
+
+    def get_root(self):
+        return self if self.parent is None else self.parent.get_root()
+
+    def __str__(self):
+        return (
+            str(self.left)
+            if self.is_leaf()
+            else f"[{str(self.left)}, {str(self.right)}]"
+        )
+
+    def add_num(self, int_node: "Node"):
+        assert self.is_leaf()
+        assert self.right is None
+        assert int_node.right is None
+        self.left += int_node.left
+
+    def __add__(self, other: "Node"):
+        temp_node = Node(None, 0)
+        temp_node.left = self
+        self.parent = temp_node
+        temp_node.right = other
+        other.parent = temp_node
+        return temp_node
+
+    def magnitude(self):
+        if self.is_leaf():
+            return self.left
+        else:
+            return 3 * self.left.magnitude() + 2 * self.right.magnitude()
+
+
+def next_node(node, direction=1):
+    stack = [node.get_root()]
+    return_next = False
+    while stack:
+        cur = stack.pop()
+        if cur.is_leaf():
+            if cur.parent == node:
+                return_next = True
+            elif return_next:
+                # print("!_>", end="")
+                return_next = False
+                return cur
+            # print(cur, end=" ")
+        else:
+            stack.extend([cur.right, cur.left][::direction])
+
+
+def next_right(node):
+    return next_node(node, 1)
+
+
+def next_left(node):
+    return next_node(node, -1)
+
+
+def reduce_rule1(root):
+    stack = [root]
+
+    while stack:
+        node = stack.pop()
+
+        # If any pair is nested inside four pairs, the leftmost such pair explodes
+        if node.is_pair() and node.depth() > 4:
+            # add to both sides
+            adj = next_left(node)
+            if adj is not None:
+                adj.add_num(node.left)
+            adj = next_right(node)
+            if adj is not None:
+                adj.add_num(node.right)
+
+            # replace pair with a zero
+            # convert into intnode
+            node.left = 0
+            node.right = None
+
+            return False
+
+        if not node.is_leaf():
+            stack.append(node.right)
+            stack.append(node.left)
+    else:
+        return True
+
+
+def reduce_rule2(root):
+    stack = [root]
+
+    while stack:
+        node = stack.pop()
+
+        # If any regular number is 10 or greater, the leftmost such regular number splits.
+        if node.is_leaf() and node.left >= 10:
+            value = node.left
+            node.left = Node(node, value // 2)
+            node.right = Node(node, value - value // 2)
+            return False
+
+        if not node.is_leaf():
+            stack.append(node.right)
+            stack.append(node.left)
+    else:
+        return True
+
+
+def fully_reduce(root):
+    done = False
+    while not done:
+        if reduce_rule1(root):
+            done = reduce_rule2(root)
+    return None  # remind myself that all is happening in-place
+
+
+if __name__ == "__main__":
+    data = aoc_utils.input_string_list()
+
+    #     data = """[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
+    # [[[5,[2,8]],4],[5,[[9,9],0]]]
+    # [6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
+    # [[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
+    # [[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
+    # [[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
+    # [[[[5,4],[7,7]],8],[[8,3],8]]
+    # [[9,3],[[9,9],[6,[4,9]]]]
+    # [[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
+    # [[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]
+    # """.strip().splitlines()
+
+    root = Node.from_string(data[0])
+
+    for other in data[1:]:
+        root += Node.from_string(other)
+        fully_reduce(root)
+
+    print(root)
+    print(root.magnitude())
+
+    magnitudes = []
+    for sn1, sn2 in itertools.permutations(data, 2):
+        root = Node.from_string(sn1)
+        root += Node.from_string(sn2)
+        fully_reduce(root)
+        magnitudes.append(root.magnitude())
+
+    print(max(magnitudes))
