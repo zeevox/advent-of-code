11ee67c7cb473d3cc3c46a110a2da6566866232a
Timothy Langer <zeevox.dev@gmail.com>
2022-12-01 05:10:27 +0000

    Solve 2022 Day 1 in Python
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

---
+++ 2022/python/Day1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import aoc_utils
+
+aoc_utils.SAMPLE = False
+
+
+def sum_reindeer(calories: str) -> int:
+    return sum(map(int, calories.splitlines()))
+
+
+if __name__ == "__main__":
+    calories_sums = list(map(sum_reindeer, aoc_utils.input_block_list()))
+    print(max(calories_sums))
+    print(sum(list(sorted(calories_sums))[-3:]))
---
+++ 2022/python/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import itertools
+from collections import Counter, defaultdict
+from functools import lru_cache, partial, reduce
+
+import more_itertools
+from sortedcontainers import SortedDict, SortedList, SortedSet
+
+import aoc_utils
+
+aoc_utils.SAMPLE = False
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    pass
