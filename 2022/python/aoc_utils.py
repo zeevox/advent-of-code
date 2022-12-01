a4faaa808f8995f2a232ce6ab180b789fcad204f
Timothy Langer <zeevox.dev@gmail.com>
2022-12-01 09:45:12 +0000

    Use better puzzle input system
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

--- 2022/python/aoc_utils.py
+++ 2022/python/aoc_utils.py
@@ -1,58 +1,33 @@
 #!/usr/bin/python3
 
-import inspect
-import os
+import argparse
+from pathlib import Path
 
-"""Whether to use a test file"""
-SAMPLE = False
-
-
-def _top_level_caller():
-    """get the filename of the original script that called this up"""
-    return inspect.stack()[-1].filename
-
-
-def get_day():
-    """
-    get the day of the python script calling this function
-    NB: no validation for filename, it is assumed to be DayXX.py
-    """
-    return os.path.basename(_top_level_caller())[3:5]
-
-
-def filepath():
-    """get the filepath of the input"""
-    return os.path.abspath(
-        f"{os.path.dirname(_top_level_caller())}/../inputs/{get_day()}{'.s' if SAMPLE else ''}.txt"
-    )
-
-
-def input():
-    """get a reference straight to the input file"""
-    return open(filepath(), "r")
+def input_file() -> Path:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', type=str, help="The filename of the puzzle input")
+    args = parser.parse_args()
+    return Path(__file__).parent.parent / "inputs" / args.filename
 
 
 def input_string():
     """read input into a string"""
-    return open(filepath(), "r").read().strip()
+    return input_file().read_text().strip()
 
 
 def input_int_list():
     """parse input into a list of ints"""
-    with open(filepath(), "r") as file:
-        return [int(line.rstrip()) for line in file]
+    return list(map(int, input_string_list()))
 
 
 def input_string_list():
     """parse input into a list of strings"""
-    with open(filepath(), "r") as file:
-        return [line.rstrip() for line in file]
+    return list(map(str.rstrip, input_string().splitlines()))
 
 
 def input_block_list():
     """input split by paragraph i.e. two newlines"""
-    with open(filepath(), "r") as file:
-        return file.read().strip().split("\n\n")
+    return input_string().split("\n\n")
 
 
 def filter_empty(li):
