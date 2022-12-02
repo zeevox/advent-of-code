8ce0c855e498ea6b5fec271d25ea1b3b29a4cfec
Timothy Langer <zeevox.dev@gmail.com>
2022-12-02 05:26:33 +0000

    Clean up 2022 Day 2 in Python
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

--- 2022/python/Day2.py
+++ 2022/python/Day2.py
@@ -1,48 +1,37 @@
 #!/usr/bin/python3
 
-import itertools
-from collections import Counter, defaultdict
-from functools import lru_cache, partial, reduce
-
-import more_itertools
-from sortedcontainers import SortedDict, SortedList, SortedSet
-
 import aoc_utils
 
-sy = {"X": 1, "Y": 2, "Z": 3}
+scoring = {"X": 1, "Y": 2, "Z": 3}
 
 lose = {"A": "Z", "B": "X", "C": "Y"}
 win = {"A": "Y", "B": "Z", "C": "X"}
 draw = {"A": "X", "B": "Y", "C": "Z"}
 
-def main(pi):
-    score = 0
-    for op, you in map(str.split, pi):
-        match [op, you]:
-            case ["A", "Y"] | ["B", "Z"] | ["C", "X"]:
-                score += sy[you] + 6
-            case ["A", "Z"] | ["B", "X"] | ["C", "Y"]:
-                score += 0 + sy[you]
-            case ["A", "X"] | ["B", "Y"] | ["C", "Z"]:
-                score += 3 + sy[you]
-    return score
+lookup = {"X": lose, "Y": draw, "Z": win}
 
-def mainb(pi):
-    score = 0
-    for op, you in map(str.split, pi):
-        match you:
-            case "X":
-                play = lose[op]
-            case "Y":
-                play = draw[op]
-                score += 3
-            case "Z":
-                play = win[op]
-                score += 6
-        score += sy[play]
-    return score
+
+def score_move(op: str, you: str) -> int:
+    if (op, you) in win.items():
+        return scoring[you] + 6
+    elif (op, you) in draw.items():
+        return scoring[you] + 3
+    elif (op, you) in lose.items():
+        return scoring[you]
+    raise Exception(f"Invalid move {op} {you}")
+
+
+def part1(puzzle_input):
+    return sum(score_move(*line.split()) for line in puzzle_input)
+
+
+def part2(puzzle_input):
+    return sum(
+        score_move(op, lookup[you][op]) for op, you in map(str.split, puzzle_input)
+    )
+
 
 if __name__ == "__main__":
     puzzle_input = aoc_utils.input_string_list()
-    print(main(puzzle_input))
-    print(mainb(puzzle_input))
+    print(part1(puzzle_input))
+    print(part2(puzzle_input))
