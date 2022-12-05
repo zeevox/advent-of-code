0d84d5ada265e2021651b74bdaa0830a8bca3690
Timothy Langer <zeevox.dev@gmail.com>
2022-12-05 06:08:25 +0000

    Solve 2022 Day 5 in Python
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

---
+++ 2022/python/Day05.py
@@ -0,0 +1,31 @@
+from aoc_utils import *
+
+
+parse_crates = lambda crates: dict(
+    enumerate(
+        [
+            [crate for crate in reversed(stack) if crate != " "]
+            for stack in zip(*crates.rstrip().splitlines()[:-1])
+        ][1:37:4]
+    )
+)
+
+
+def pm(mv):
+    _, c, _, s, _, e = mv.split()
+    return int(c), int(s) - 1, int(e) - 1
+
+
+def main(part1: bool = False) -> str:
+    crates, movements = input_block_list()
+    stacks: dict[int, list[str]] = parse_crates(crates)
+    movements_parsed = list(map(pm, movements.split("\n")))
+    for count, start, end in movements_parsed:
+        stacks[end] += stacks[start][-count:][:: -1 if part1 else 1]
+        stacks[start] = stacks[start][:-count]
+    return "".join(stacks[i][-1] for i in range(9))
+
+
+if __name__ == "__main__":
+    print(main(part1=True))
+    print(main(part1=False))
