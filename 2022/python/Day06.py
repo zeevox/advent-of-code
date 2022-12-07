5f00947b0665ad00113f69a911f659e647b2b378
Timothy Langer <zeevox.dev@gmail.com>
2022-12-07 05:40:18 +0000

    Solve 2022 Day 6 in Python
    Signed-off-by: Timothy Langer <zeevox.dev@gmail.com>

---
+++ 2022/python/Day06.py
@@ -0,0 +1,15 @@
+import more_itertools
+
+from aoc_utils import *
+
+
+def main(buffer, n: int = 4):
+    for i, ls in enumerate(more_itertools.windowed(buffer, n)):
+        if len(set(ls)) == n:
+            return i + n
+
+
+if __name__ == "__main__":
+    inp = input_string()
+    print(main(inp, 4))
+    print(main(inp, 14))
